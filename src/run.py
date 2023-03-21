c504f5b3bc4b5471d4aab8fe8c29cf92a4da428d
Luba <luciano.eq@outlook.com>
2023-03-21 09:07:20 -0300

    best-model: tunning arima model.

--- src/run.py
+++ src/run.py
@@ -1,14 +1,28 @@
-import yaml
-from data_prep.prep import prep_data
-
-
-def run():
-    with open("params.yaml", "r") as f:
-        config = yaml.safe_load(f)
-
-    train_path = config["data"]["train_path"]
-    prep_data(train_path)
-
+import itertools
+import subprocess
 
 if __name__ == "__main__":
-    run()
+    # Automated grid search experiments
+    arima_max_iter = [100, 200, 300]
+    arima_n_fits = [10, 50, 100]
+    arima_m = [1, 12, 52]
+
+    # Iterate over all combinations of hyperparameter values.
+    for x_arima_m, x_arima_n_fits, x_arima_max_iter in itertools.product(
+        arima_m, arima_n_fits, arima_max_iter
+    ):
+        subprocess.run(
+            [
+                "dvc",
+                "exp",
+                "run",
+                "-f",
+                "--queue",
+                "--set-param",
+                f"train.arima_n_fits={x_arima_n_fits}",
+                "--set-param",
+                f"train.arima_maxiter={x_arima_max_iter}",
+                "--set-param",
+                f"train.arima_m={x_arima_m}",
+            ]
+        )
--- src/training/train.py
+++ src/training/train.py
@@ -20,40 +20,99 @@ class Trainer:
         metrics_path: str | Path,
         arima_model_path: str | Path,
         sub_name: str,
+        arima_n_fits: int,
+        arima_max_iter: int,
+        m: int,
     ) -> None:
         self.train_path = train_path
         self.test_path = test_path
         self.metrics_path = metrics_path
         self.arima_model_path = arima_model_path
         self.sub_name = sub_name
+        self.arima_n_fits = arima_n_fits
+        self.arima_max_iter = arima_max_iter
+        self.m = m
+        self.model = None
 
     def train_baseline(self) -> None:
         # train data
         train_data = pl.read_parquet(self.train_path)
-
         # imputing missing values
         train_data_interpolated = train_data.interpolate()
-        train, test = train_test_split(
-            train_data_interpolated["sleep_hours"], train_size=0.9
+        train_data_interpolated = train_data_interpolated.with_columns(
+            [
+                pl.col("date").dt.year().alias("year"),
+                pl.col("date").dt.day().alias("day"),
+                pl.col("date").dt.month().alias("month"),
+                pl.col("date").dt.week().alias("week"),
+                pl.col("date").dt.weekday().alias("weekday"),
+            ]
+        ).with_columns(
+            [
+                pl.when(
+                    pl.col("date").is_between(
+                        datetime(2017, 9, 27), datetime(2018, 6, 12)
+                    )
+                )
+                .then(pl.col("sleep_hours") / 2)
+                .otherwise(pl.col("sleep_hours"))
+                .alias("sleep_hours")
+            ]
         )
+        train_data_interpolated = train_data_interpolated.groupby("week").agg(
+            pl.mean("sleep_hours").alias("sleep_hours")
+        )
+        train_data_interpolated = train_data_interpolated.sort("week")
+        train, test = train_test_split(
+            train_data_interpolated["sleep_hours"], train_size=0.7
+        )
+
+        # ARIMA_model = pm.auto_arima(
+        #     train,
+        #     start_p=1,
+        #     start_q=1,
+        #     start_P=1,
+        #     start_Q=1,
+        #     max_p=5,
+        #     max_q=5,
+        #     max_P=5,
+        #     max_Q=5,
+        #     m=52,
+        #     seasonal=True,
+        #     stepwise=True,
+        #     suppress_warnings=True,
+        #     D=0,
+        #     random_state=42,
+        #     # max_D=3,
+        #     error_action="ignore",
+        # )
 
         ARIMA_model = pm.auto_arima(
             train,
-            start_p=1,
-            start_q=1,
-            start_P=1,
-            start_Q=1,
-            max_p=5,
-            max_q=5,
-            max_P=5,
-            max_Q=5,
+            start_p=0,
+            d=1,
+            start_q=0,
+            max_p=6,
+            max_d=6,
+            max_q=6,
+            start_P=0,
+            D=0,
+            start_Q=0,
+            max_P=6,
+            max_D=6,
+            max_Q=6,
+            m=self.m,
             seasonal=True,
+            error_action="warn",
+            trace=True,
+            supress_warnings=True,
             stepwise=True,
-            suppress_warnings=True,
-            D=10,
-            max_D=10,
-            error_action="ignore",
+            random_state=42,
+            n_fits=self.arima_n_fits,
+            maxiter=self.arima_max_iter,
         )
+
+        self.model = ARIMA_model
 
         # Create predictions for the future, evaluate on test
         preds, conf_int = ARIMA_model.predict(
@@ -61,8 +120,8 @@ class Trainer:
         )
 
         # saving the model
-        with open(self.arima_model_path, "wb") as pkl:
-            pickle.dump(ARIMA_model, pkl)
+        # with open(self.arima_model_path, "wb") as pkl:
+        #     pickle.dump(ARIMA_model, pkl)
 
         # saving the metrics
         with open(self.metrics_path, "w") as f:
@@ -77,10 +136,14 @@ class Trainer:
         )
 
         # loading trained model
-        with open(self.arima_model_path, "rb") as pkl:
-            pickle_preds, conf_int = pickle.load(pkl).predict(
-                n_periods=periods_to_predict.shape[0], return_conf_int=True
-            )
+        # with open(self.arima_model_path, "rb") as pkl:
+        #     pickle_preds, conf_int = pickle.load(pkl).predict(
+        #         n_periods=periods_to_predict.shape[0], return_conf_int=True
+        #     )
+
+        pickle_preds, conf_int = self.model.predict(
+            n_periods=periods_to_predict.shape[0], return_conf_int=True
+        )
 
         submission_df = pl.DataFrame(
             {"date": periods_to_predict, "sleep_hours": pickle_preds}
@@ -100,7 +163,7 @@ class Trainer:
             {"sleep_hours_right": "sleep_hours"}
         )
         submission_final.with_columns([pl.col("date").cast(pl.Date)]).write_csv(
-            "data/pog-sleep-data/subs/{self.sub_name}.csv"
+            f"data/pog-sleep-data/subs/{self.sub_name}.csv"
         )
 
 
@@ -113,6 +176,9 @@ if __name__ == "__main__":
     metrics_path = config["train"]["metrics_path"]
     arima_model_path = config["train"]["arima_model_path"]
     sub_name = config["train"]["sub_name"]
+    arima_n_fits = config["train"]["arima_n_fits"]
+    arima_max_iter = config["train"]["arima_maxiter"]
+    arima_m = config["train"]["arima_m"]
 
     trainer = Trainer(
         train_path=train_path,
@@ -120,6 +186,9 @@ if __name__ == "__main__":
         metrics_path=metrics_path,
         arima_model_path=arima_model_path,
         sub_name=sub_name,
+        arima_n_fits=arima_n_fits,
+        arima_max_iter=arima_max_iter,
+        m=arima_m,
     )
     trainer.train_baseline()
     trainer.create_submission()
